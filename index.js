5ed9f5b05420c119abd915c786aa5211f9b0bd9f
arbabgillani <arbabzaman060@gmail.com>
2023-08-02 17:52:31 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+"use strict";
+//arithmetic operators (+ ,- , * , / , %)
+//addition +
+var a = 1;
+var a1 = 2;
+var Add = a + a1;
+console.log("add:");
+console.log(Add);
+//substraction -
+var Substract = a - a1;
+console.log("Substraction:");
+console.log(Substract);
+//multiplication *
+var Multiplication = a * a1;
+console.log("Multiplication :");
+console.log(Multiplication);
+//Division  /
+var Division = a / a1;
+console.log("Divison :");
+console.log(Division);
+//Modulus %
+var Modulus = a % a1;
+console.log("Modulus:");
+console.log(Modulus);
+//What is the value of (8 - 3) * (4 + 2)?
+const result = (8 - 3) * (4 + 2);
+console.log(result);
+//Evaluate 2 ** 3, which represents 2 raised to the power of 3.
+const result2 = 2 ** 3;
+console.log(result2);
+//If x = 7 and y = 4, find the result of x + y * 2
+var x = 7;
+var y = 4;
+var result3 = x + y * 2;
+console.log("result:");
+console.log(result3);
---
+++ index.ts
@@ -0,0 +1,52 @@
+//arithmetic operators ( addition + , substraction- ,multiplication * , division / ,  modulus % , exponent **)
+//addition +
+var a1=9;
+var a2=72;
+var Add=a1+a2;
+console.log("add:");
+console.log(Add);
+//substraction -
+var b1=9;
+var b2=0;
+var Substract=b1-b2;
+console.log("Substraction:");
+console.log(Substract);
+//multiplication *
+var c1=8;
+var c2=9;
+var Multiplication=c1*c2;
+console.log("Multiplication :");
+console.log(Multiplication);
+//Division  /
+var d1=4;
+var d2=5;
+var Division=d1-d2;
+console.log("Divison :");
+console.log(Division);
+//Modulus %
+var e1=2;
+var e2=6;
+var Modulus=e1%e2;
+console.log("Modulus:");
+console.log(Modulus);
+//exponent **
+var f1=7;
+var f2 =5;
+var Exponent=f1**f2;
+console.log("Exponent :");
+console.log(Exponent);
+//What is the value of (8 - 3) * (4 + 2)?
+const result=(8 - 3) * (4 + 2);
+console.log("result:")
+console.log(result);
+//Evaluate 2 ** 3, which represents 2 raised to the power of 3.
+const result2=2 ** 3;
+console.log("result is");
+console.log(result2);
+//If x = 7 and y = 4, find the result of x + y * 2
+var x=7;
+var y=4;
+var result3=x+y*2;
+console.log("result:");
+console.log(result3);
+//
